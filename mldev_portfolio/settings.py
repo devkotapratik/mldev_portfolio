d83f284d0e5345718968ac5d847faa672dd6d667
Pratik Devkota <pratik.devkota10@gmail.com>
2024-06-24 20:42:58 -0400

    Added onrender.com to allowed hosts for hosting in render

--- mldev_portfolio/settings.py
+++ mldev_portfolio/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "devkotapratik.onrender.com",
+]
 
 
 # Application definition
